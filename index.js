c7335ce54b717d3074d159c613dcd8ed5eafeeee
Alejandro2097 <alejo12381@hotmail.es>
2021-09-02 22:02:47 -0500

    tercer comit xdxdxd

--- index.js
+++ index.js
@@ -3,15 +3,32 @@ const { inquirerMenu,
     leerInput,
     listadoTareasBorrar,
     confirmar,
-    mostrarListadoCheck} = require("./helpers/inquirer")
+    mostrarListadoCheck} = require("./helpers/inquirer");
+const Busquedas = require("./models/busquedas");
 
 const main = async() =>{
+
+    const busquedas =  new Busquedas();
     let opt;
     do{
         opt = await inquirerMenu();
         switch(opt){
             case 1:
-                console.log('Selecciono buscar ciudad');
+                //mostrar mensaje 
+                const lugar = await leerInput('Ciudad: ');
+                await busquedas.ciudad(lugar);
+                //Buscar los lugares
+
+                //clima
+                
+                //mostrar resultados
+                console.log('\nInformacion de la ciudad\n'.green);
+                console.log('Ciudad: ', );
+                console.log('Lat: ', );
+                console.log('Log: ', );
+                console.log('Temperatura: ', );
+                console.log('Minima: ', );
+
             break;
 
             case 2:
--- models/busquedas.js
+++ models/busquedas.js
@@ -1,5 +1,37 @@
+const axios = require('axios');
+
 class Busquedas{
-    
+    historial = ['Tegucigalpa', 'Madrid', 'San jose'];
+
+    constructor(){
+        //TODO: leer DB si existe 
+    }
+    async ciudad( lugar = ''){
+
+        try{
+            //peticion http
+            const instance = axios.create({
+                baseURL:  `https://api.mapbox.com/geocoding/v5/mapbox.places/${lugar}.json`,
+                params:{
+                    'access_token' : 'pk.eyJ1Ijoia3JhdXNlcjczIiwiYSI6ImNrdDNta2ZsZDB5dTMydG54c2t2ZGgycHQifQ.sfg8B1rxqzdECb2U6FB-eQ',
+                    'limit': 5,
+                    'language': 'es'
+                }
+            });
+        
+            const resp = await instance.get();
+           
+            console.log(resp.data);
+
+            return []; 
+        }catch(error){
+            return[];
+        }   
+        
+
+        return [];// retornar los lugares
+
+    }
 }
 
 module.exports = Busquedas;
